1f08e23537a7fbae19b910c55896eec066ac4464
Jimmy Fung <jimmythefung@yahoo.com>
2017-07-24 01:08:43 -0500

    Workout part 1 working

--- Workout/app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
+++ Workout/app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
@@ -2,10 +2,11 @@ package com.hfad.workout;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -25,6 +26,21 @@ public class WorkoutDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        View view = getView();
+        if (view != null){
+            Workout workout = Workout.workouts[(int) workoutId];
+
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            title.setText(workout.getName());
+
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
     }
 
     public void setWorkout(long id){
